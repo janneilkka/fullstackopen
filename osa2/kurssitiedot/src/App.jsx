184f9a7bc517d5d7688bfffc753951b105c0c958
Janne Koivisto <49063172+janneilkka@users.noreply.github.com>
2024-10-06 22:28:25 +0300

    Exercises 2.1-2.5

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,81 @@
+const Header = (props) => <h1>{props.text}</h1>;
+
+const Course = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course) => {
+        return (
+          <>
+            <h2>{course.name}</h2>
+            {course.parts.map((part) => {
+              return (
+                <ul>
+                  <li key={part.id}>
+                    {part.name}: {part.exercises}
+                  </li>
+                </ul>
+              );
+            })}
+            <p>The total of the exercises is: {course.parts.reduce((acc, item) => acc + item.exercises, 0)}</p>
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <Header text="Web development curriculum" />
+      <Course courses={courses} />
+    </div>
+  );
+};
+
+export default App;
